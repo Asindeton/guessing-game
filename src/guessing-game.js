ede5c98bc6ce02dcc558671627636c4c37dc9330
Дмитрий Леганьков <you@example.com legankov95@gmail.comgit config --global user.name Asindetongit commit -m 'test cv'$ git config --global user.email you@example.com>
2019-10-12 13:08:02 +0300

    feat:solvd guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.workNumber = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.workNumber = Math.round(this.min + (this.max - this.min) / 2);
+        return this.workNumber;
     }
 
     lower() {
-
+        this.max = this.workNumber;
     }
 
     greater() {
-
+        this.min = this.workNumber;
     }
 }
 
